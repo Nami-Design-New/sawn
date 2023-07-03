c38c05e06d0b7b44bf6de57bb3428328282831bd
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2023-07-03 15:36:34 +0300

    1

--- app.js
+++ app.js
@@ -18,4 +18,48 @@ window.onscroll = function() {
     started = true;
   }
 };
+//------------------------end animate--------------------------//
 
+//------------------phone images slider------------------------//
+var phoneImgs = new Swiper(".phoneImgs", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  spaceBetween: 60,
+  speed: 1000,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  loop: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 5,
+    stretch: 10,
+    depth: 200,
+    modifier: 1.2,
+    slideShadows: true
+  },
+  navigation: {
+    nextEl: ".appSliderNext",
+    prevEl: ".appSliderPrev"
+  }
+});
+//---------------nav-links-highlight---------------------//
+let navLinks = document.querySelectorAll(".nav-link");
+let sections = document.querySelectorAll(".sec");
+let current;
+window.addEventListener("scroll", () => {
+  sections.forEach(sec => {
+    let secTop = sec.offsetTop;
+    if (pageYOffset >= secTop - 110) {
+      current = sec.getAttribute("id");
+    }
+  });
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.classList.contains(current)) {
+      link.classList.add("active");
+    }
+  });
+});
